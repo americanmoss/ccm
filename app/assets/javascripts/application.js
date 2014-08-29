b05e8790239c224e8f08e241fcd08207e64f14aa
Moss Amer <mookman74@gmail.com>
2014-08-28 22:59:46 -0400

    Added google maps API support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,3 +32,141 @@ $(function() {
     }
   });
 });
+
+
+function initialize() {
+
+	var styles = [
+	    {
+	        "featureType": "administrative",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "saturation": -100
+	            },
+	            {
+	                "lightness": 20
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "road",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "saturation": -100
+	            },
+	            {
+	                "lightness": 40
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "water",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "on"
+	            },
+	            {
+	                "saturation": -10
+	            },
+	            {
+	                "lightness": 30
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "landscape.man_made",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "simplified"
+	            },
+	            {
+	                "saturation": -60
+	            },
+	            {
+	                "lightness": 10
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "landscape.natural",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "simplified"
+	            },
+	            {
+	                "saturation": -60
+	            },
+	            {
+	                "lightness": 60
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "poi",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            },
+	            {
+	                "saturation": -100
+	            },
+	            {
+	                "lightness": 60
+	            }
+	        ]
+	    },
+	    {
+	        "featureType": "transit",
+	        "elementType": "all",
+	        "stylers": [
+	            {
+	                "visibility": "off"
+	            },
+	            {
+	                "saturation": -100
+	            },
+	            {
+	                "lightness": 60
+	            }
+	        ]
+	    }
+	]
+
+	var styledMap = new google.maps.StyledMapType(styles, {name: "Area 405"});
+
+	var mapOptions = {
+		center: new google.maps.LatLng(39.306985, -76.618332),
+		zoom: 15,
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		}
+	};
+	
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	map.mapTypes.set('map_style', styledMap);
+	map.setMapTypeId('map_style');
+
+	var icon = 'http://www.clker.com/cliparts/c/9/m/4/B/d/google-maps-grey-marker-w-shadow-th.png'
+
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(39.306985, -76.610332),
+		map: map,
+		icon: icon,
+		title:"Area 405"
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
